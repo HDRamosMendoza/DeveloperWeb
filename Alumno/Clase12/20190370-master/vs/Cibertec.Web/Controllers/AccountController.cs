942ddd02e8ed4f9e49c74547bcb62fcbdd0d4805
H. Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-08-05 22:15:32 -0500

    Tercera parte
    Tercera parte

---
+++ Alumno/Clase12/20190370-master/vs/Cibertec.Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cibertec.UnitOfWork;
+
+namespace Cibertec.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController(IUnitOfWork unit ): base(unit)
+        {
+        }        
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Alumno/Clase12/20190370-master/vs/Cibertec.Web/Models/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cibertec.Web.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        //public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
