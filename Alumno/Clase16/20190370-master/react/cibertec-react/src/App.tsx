194c9db2a3e4619d6c13a4ec76bf575c5735f194
H. Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-08-19 22:17:11 -0500

    Solucionar problema de creacion de proyectos
    Solucionar problema de creacion de proyectos

--- Alumno/Clase16/20190370-master/react/cibertec-react/src/App.tsx
+++ Alumno/Clase16/20190370-master/react/cibertec-react/src/App.tsx
@@ -3,13 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import {Table} from "./components/Table";
 
+interface IProducto {
+  nombre: string;
+  precio: number;
+}
+
+interface ICliente {
+  nombre: string;
+  apellido: string;
+  edad: number;
+}
+
 const App: React.FC = () => {
   const array = [1,2,3,4,5,6,7];
   const array1:[10] = [10]
   const arrayTables = [];
 
   for (let index= 0; index < 50; index++) {
-    arrayTables.push(<Table title={`Tabla ${index}`}></Table>)
+    arrayTables.push(<Table title={`Tabla ${index}`} key={index}></Table>)
   }
 
   //const tableViews = array.map((index) => {
--- Alumno/Clase16/20190370-master/react/cibertec-react/src/components/Table.tsx
+++ Alumno/Clase16/20190370-master/react/cibertec-react/src/components/Table.tsx
@@ -2,11 +2,51 @@ import React from "react";
 
 interface IProps {
     title: string;
-    data?:[];
+    data?: any[];
 }
 
+interface ITHeadProps {
+    headers: string[];
+}
+
+interface ITBodyProps {
+    data: any[];
+}
+
+const THeader: React.FC<ITHeadProps> = (props) => {
+    const listaCabeceras:any[] = [];
+    // debugger
+    // console.log("estos son los headers: ", props.headers);
+
+    for(const header of props.headers) {
+        listaCabeceras.push(<th key={header}>
+            {header}
+        </th>)
+    }
+
+    return <thead>
+        <tr>
+            {listaCabeceras}
+        </tr>
+    </thead>
+}
+
+
+
 export const Table: React.FC<IProps> = (props) => {
-    return <p>
-        Mi componente tabla {props.title}
-    </p>
+    if(!props.data) {
+        return <p>
+            No hay data
+        </p>
+    }
+
+    const listaHeaders = Object.entries(props.data[0]).map(
+        ([a, v]) => {
+            return a;
+        }
+    )
+
+    return <table>
+        <THeader headers={listaHeaders}></THeader>
+    </table>
 }
