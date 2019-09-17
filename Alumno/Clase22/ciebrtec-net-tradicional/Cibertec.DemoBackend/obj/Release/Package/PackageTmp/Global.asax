37213fce2e93ab8505eb9565f3e62c28c7fe1e14
H. Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-09-16 21:36:28 -0500

    Finalizacion de la clase
    Finalizacion de la clase

---
+++ Alumno/Clase22/ciebrtec-net-tradicional/Cibertec.DemoBackend/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cibertec.DemoBackend.WebApiApplication" Language="C#" %>
---
+++ Alumno/Clase22/ciebrtec-net-tradicional/Cibertec.Repositories.Dapper/Northwind/SupplierRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cibertec.Models;
+using Cibertec.Repositories.Northwind;
+using Dapper;
+using System.Data.SqlClient;
+using Cibertec.Models.ViewModels;
+
+namespace Cibertec.Repositories.Dapper.Northwind
+{
+    public class SupplierRepository : Repository<Supplier>, ISupplierRepository
+    {
+        public SupplierRepository(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+}
